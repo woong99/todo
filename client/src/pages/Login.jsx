e1d97cf7e915840a0d8fe764f3aad9b8ef598ca0
woong99 <dndry1072@naver.com>
2022-03-29 21:52:47 +0900

    todo

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 const Login = () => {
+  const [id, setId] = useState('');
+
+  const onChange = (e) => {
+    setId(e.target.value);
+  };
+  const clickId = () => {
+    console.log(id);
+  };
+
+  const onClickLogin = () => {
+    axios
+      .post('/user_inform/onLogin', null, {
+        params: {
+          user_id: id,
+        },
+      })
+      .then((res) => console.log(res))
+      .catch();
+  };
+
   // 페이지 렌더링 후 가장 처음 호출되는 함수
   const click = () => {
     axios
@@ -14,8 +34,9 @@ const Login = () => {
     <div>
       Login
       <button onClick={click}>버튼</button>
-      <button onClick={click}>버튼</button>
-      <button onClick={click}>버튼</button>
+      <input onChange={onChange} />
+      <button onClick={onClickLogin}>클릭</button>
+      <input />
     </div>
   );
 };
--- routes/user_inform.js
+++ routes/user_inform.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
+const util = require('util');
 
-router.get('/login', (req, res) => {
-  // 임시로 값을 넣어 주었다.
-  res.send({ data: '1231232' });
+router.post('/onLogin', (req, res) => {
+  // console.log(`= = = > req : ${util.inspect(req)}`);
+
+  const user_id = req.query.user_id;
+
+  // 입력된 id와 동일한 id가 있는지 확인
+  const sql1 = 'SELECT COUNT(*) AS result FROM user_inform WHERE user_id = ?';
+  db.query(sql1, user_id, (err, data) => {
+    console.log('---------------------------');
+    // console.log(db);
+    console.log(data[0].result);
+  });
 });
+// router.get('/login', (req, res) => {
+//   // 임시로 값을 넣어 주었다.
+//   res.send({ data: '1231232' });
+// });
 
 module.exports = router;
